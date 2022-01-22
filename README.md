9feb9eacd74bea22a2622da2f206119526fb8793
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2022-01-22 18:39:10 +0000

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Whereby-API
 
 This is a quick package to start a whereby. Features are:
-* Create a meeting
+* Create a meeting# node-stripe-api
--- app.js
+++ app.js
@@ -2,42 +2,24 @@ require("dotenv").config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const axios = require('axios')
 const bodyParser = require('body-parser')
-
 const PORT = process.env.PORT || 4000
-let wherebyUrl = 'https://api.whereby.dev/v1'
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json(), cors())
 app.options('*', cors());
 
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.send('🟢 Stripe working good bro.')
 })
 
-app.post('/createRoom', async (req, res) => {
-  await axios.post(`${wherebyUrl}/meetings`, JSON.stringify(req.body), {
-    headers: {
-      Authorization: `Bearer ${process.env.WHEREBY_API_KEY}`,
-      "Content-Type": "application/json",
-    }
-  })
-    .then(response => {
-      res.send({
-        status: 200,
-        meeting: response.data
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.send({
-        status: 400,
-        message: 'Cannot create your whereby meeting',
-        error: err
-      })
-    })
+app.post('/create-payment-intent', async (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create(req.body);
+  res.send(paymentIntent)
 })
 
-app.listen(PORT)
+app.listen(PORT, () => console.log('Launched on port ' + PORT))
